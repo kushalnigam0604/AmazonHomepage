51efe60a9934be8ebc048139ad9718ca4ca932b7
kushalnigam0604 <nigamkushal0@gmail.com>
2022-02-26 16:50:46 +0530

    practice of DOM

---
+++ ProductPage/index.js
@@ -0,0 +1,2 @@
+var ele = document.getElementsByClassName('allColumnImg');
+console.log(ele[0].children[0].children[0]);
---
+++ index.js
@@ -0,0 +1,5 @@
+var a = document.links;
+console.log(a);
+for(let i = 0; i<a.length; i++){
+    console.log(a[i].text);
+}
