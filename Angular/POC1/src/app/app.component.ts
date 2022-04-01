4b4b2f6377c3c9adbc6bb02f5fae44e3f06eb7b3
kushalnigam0604 <nigamkushal0@gmail.com>
2022-04-01 19:26:22 +0530

    concepts of component, modules, data binding

--- Angular/POC1/src/app/app.component.ts
+++ Angular/POC1/src/app/app.component.ts
@@ -7,4 +7,7 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'POC1';
+  months = ["jan","feb","march","apr","may","june","july","aug","sep","oct","nov","dec"];
+  isAvailable = false;
+  istrue = true;
 }
