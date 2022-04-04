65f686a1c915b460e19abdba8d54add0e5b369f9
kushalnigam0604 <nigamkushal0@gmail.com>
2022-04-04 19:03:10 +0530

    Concept of routing

--- Angular/POC1/src/app/app.module.ts
+++ Angular/POC1/src/app/app.module.ts
@@ -1,22 +1,30 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+// import { RouterModule } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FirstComponent } from './first/first.component';
 import { ChangeTextDirective } from './change-text.directive';
 import { SqrtPipe } from './app.sqrt';
+import { RouteComponentComponent } from './route-component/route-component.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     FirstComponent,
     ChangeTextDirective,
-    SqrtPipe
+    SqrtPipe,
+    RouteComponentComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    // RouterModule.forRoot([
+    //   {
+    //     path: ""
+    //   }
+    // ])
   ],
   providers: [],
   bootstrap: [AppComponent]
