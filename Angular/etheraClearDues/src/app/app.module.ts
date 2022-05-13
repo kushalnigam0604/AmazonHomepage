7e807497266b7a1354a775da468063660dcd9c37
kushalnigam0604 <nigamkushal0@gmail.com>
2022-05-13 18:20:49 +0530

    create structure and add style

--- Angular/etheraClearDues/src/app/app.module.ts
+++ Angular/etheraClearDues/src/app/app.module.ts
@@ -3,14 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ApplicationComponent } from './application/application.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ApplicationComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
